9a28684b71d9b943163914170ffbe175605e70b6
yokoyama443 <103204303+yokoyama443@users.noreply.github.com>
2024-08-31 06:49:43 +0900

    Update setup.sh (#13)

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "外部パッケージをインストールします..."
+pip install keyboard pyaudio textual python-dotenv openai gpiozero
+echo "外部パッケージをインストールしました"
+
 cp ./start-origin.sh ./start.sh
 echo "start.sh を作成しました"
 chmod +x ./start.sh
